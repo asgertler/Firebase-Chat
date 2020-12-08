34ff5a3b3d90be28b5d275efab4a7707b3e7b492
Aaron Gertler <aaron.gertler@gmail.com>
2020-12-07 18:08:01 -0600

    initialized firestore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
+import './App.css'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCtFqdQBWyvbtkW8ilIUL0gLSZAWiBEYys",
+  authDomain: "fir-chat-5a132.firebaseapp.com",
+  projectId: "fir-chat-5a132",
+  storageBucket: "fir-chat-5a132.appspot.com",
+  messagingSenderId: "982749765635",
+  appId: "1:982749765635:web:c370d7a7964779dc846e36",
+  measurementId: "G-YM1N2ZHMDL"
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
